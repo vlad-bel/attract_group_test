6784a984331cdc6b2b68122cc0afb7199ed74620
belenkov <belenkov@surfstudio.ru>
2020-11-30 12:33:21 +0500

    add details screen ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:attract_group_test/ui/screen/details_screen/details_screen.dart';
+import 'package:attract_group_test/ui/screen/details_screen/details_screen_pages.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // home: ListScreen(),
+      home: DetailsScreenPages(),
     );
   }
 }
